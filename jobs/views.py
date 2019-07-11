3d1b9700dd6fe8b2b3f7185b29b9d931ddbc294f
Jethet <henriette.hettinga@gmail.com>
2019-07-11 12:27:35 +0200

    Add jobs list from database to home page

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+# this gets the jobs from the database and turns them into objects:
+from .models import Job
 
+
+# the homepage is in the jobs app, because it shows all jobs
 def home(request):
-    return render(request, 'jobs/home.html') # the homepage is in the jobs
-                                            # app, because it shows all jobs
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs})
+    
