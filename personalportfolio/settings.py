fedab1cc9b4abe5d04b45147207fc260050e3029
Jethet <henriette.hettinga@gmail.com>
2019-07-11 17:45:59 +0200

    Add static root and url to settings.py

--- personalportfolio/settings.py
+++ personalportfolio/settings.py
@@ -127,6 +127,14 @@ STATIC_URL = '/static/'
 
 # To save media files:
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'personalportfolio/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
