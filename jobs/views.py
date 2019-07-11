9492ba43d5e68407194c796574893506011d9ca1
Jethet <henriette.hettinga@gmail.com>
2019-07-11 12:39:52 +0200

    Use loop to get all job views on home page and create first jobs

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 # this gets the jobs from the database and turns them into objects:
 from .models import Job
 
@@ -7,4 +8,3 @@ from .models import Job
 def home(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
-    
