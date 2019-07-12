9ec6a2d184379f1eca9d76118d5f6c59afad734a
Jethet <henriette.hettinga@gmail.com>
2019-07-12 15:19:42 +0200

    Correct error

--- personalportfolio/settings.py
+++ personalportfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '47fzr#90i=Bog59(dbx@1&zhiu7v9%c2(2*m60_lf5hi_e46xq32k)1'
+SECRET_KEY = '47fzr#90i=(dbx@1&zhiu7v9%c2(2*m60_lf5hi_e46xq32k)1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
